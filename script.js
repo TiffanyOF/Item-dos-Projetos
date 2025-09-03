68d86290036c55672c5d1989f393e5fdd763f40c
Tiffany <tiffany3005of@gmail.com>
2025-09-02 21:30:26 -0300

    .

---
+++ script.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor:true,
+    
+    centeredSlides:true,
+    initialSlide: 1,
+    speed: 600,
+    preventClicks: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate:0,
+        stretch: 80,
+        depth: 350,
+        modifier: 1,
+        slideShadows: true,
+    },
+    on: {
+        click(event) {
+            swiper.slideTo(this.clickedIndex);
+        },
+    },
+    pagination: {
+        el:".swiper-pagination",
+    },
+    loop:true,
+    navigation:{
+        nextEl : ".swiper-button-next",
+        prevEl : ".swiper-button-prev"
+    },
+});
